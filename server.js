73189384bd224548c80f8ef3aed55be3ac6c95b2
imtiazmahamood <120158899+imtiazmahamood@users.noreply.github.com>
2026-06-20 22:01:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname));
-app.listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
